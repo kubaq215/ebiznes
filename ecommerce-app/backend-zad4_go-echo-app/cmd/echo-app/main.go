8fbb4e30679a3d9b834725d69c785da780bb0247
Jakub Samel <kubaq215@interia.pl>
2024-06-24 01:13:57 +0200

    chore: Update CORS configuration to allow all origins in backend and update Node.js version in frontend Dockerfile

--- ecommerce-app/backend-zad4_go-echo-app/cmd/echo-app/main.go
+++ ecommerce-app/backend-zad4_go-echo-app/cmd/echo-app/main.go
@@ -19,7 +19,7 @@ func main() {
 
     // CORS middleware setup to allow requests from the React frontend
     e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-        AllowOrigins: []string{"http://super-stronka-na-ebiznes.azurewebsites.net"}, // URL of the React app
+        AllowOrigins: []string{"http://super-stronka-na-ebiznes.azurewebsites.net", "*"}, // URL of the React app and all origins
         AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
         AllowHeaders: []string{"Content-Type", "Authorization"},
     }))
