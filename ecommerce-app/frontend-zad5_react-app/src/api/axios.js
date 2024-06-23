8e65ef20f31a34ca8caee392377c52649a14776a
Jakub Samel <kubaq215@interia.pl>
2024-06-23 22:42:06 +0200

    changed to work on azure

--- ecommerce-app/frontend-zad5_react-app/src/api/axios.js
+++ ecommerce-app/frontend-zad5_react-app/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/', // Base URL is taken from environment variables
+  baseURL: 'http://super-api-na-ebiznes.azurewebsites.net/', // Base URL is taken from environment variables
   headers: {
     'Content-Type': 'application/json'
   }
